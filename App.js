4055be34e459cfddcd339ddfb21f016df706d858
Erica Carballo <airwreckuh101@gmail.com>
2021-07-07 16:03:49 -0700

    moved containers and added different colors

--- App.js
+++ App.js
@@ -1,18 +1,31 @@
 import * as React from "react";
-import { Text, View, StyleSheet, Image } from "react-native";
+import { Text, View, StyleSheet, TouchableOpacity, Image } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.photoContainer}>
-        <Image source={require("./assets/perry.png")} style={styles.photo} />
-      </View>
+
       <View style={styles.nameContainer}>
-        <Text style={styles.nameAndPronouns}>Perry (she/her)</Text>
+        <Text style={styles.name}>Erica</Text>
+        <Text style = {styles.pronouns}> (she / her)</Text>
       </View>
+
+      <View style={styles.photoContainer}>
+        <Image source={require("./assets/bunny1.jpeg")} style={styles.photo} />
+      </View>
+
+      <TouchableOpacity
+        onPress={() => alert('How do trees access the internet? ---- They log on.')}
+        style={styles.dontClickButton}>
+        <Text style={{ fontSize: 20, color: '#fff' }}>Don't click me :)</Text>
+      </TouchableOpacity>
+
       <View style={styles.blurbContainer}>
-        <Text style={styles.blurb}>chirp chirp i'm a parakeet</Text>
+        <Text style={styles.blurb}>Hello ! My name is Erica and I enjoy figure skating, hiking, and programming. </Text>
+        
       </View>
+
+
     </View>
   );
 }
@@ -20,33 +33,56 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "pink",
   },
+
   photoContainer: {
-    flex: 3,
+    flex: 2,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "yellow",
     padding: 30,
+    backgroundColor: "#82EEFD",
   },
+
   nameContainer: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    
   },
+
   blurbContainer: {
     flex: 2,
     justifyContent: "center",
     alignItems: "center",
+    backgroundColor: "#99EDC3",
+    
   },
+
   photo: {
     resizeMode: "contain",
     width: "90%",
     padding: 50,
   },
-  nameAndPronouns: {
+
+  name: {
     fontSize: 50,
   },
-  blurb: {
+
+  pronouns:{
     fontSize: 20,
   },
+
+  blurb: {
+    textAlign: "center",
+    padding: 30,
+    fontSize: 20,
+  },
+
+  dontClickButton:
+  {
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#BCA0DC", 
+  }
 });
